a42e54ffcab3bae25884025a4833826191620a94
Ailya-Shah <rhooke44@gmail.com>
2025-09-09 18:55:47 +0500

    Implement utilities module

---
+++ src/utilities-test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "utilities.h"
+
+using namespace std;
+
+int main() {
+    int a = 2, b = 3;
+    cout << "add(" << a << ", " << b << ") = " << add(a, b) << endl;
+    return 0;
+}
